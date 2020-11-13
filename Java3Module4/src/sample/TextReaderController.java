690c030d582674d68fe5efdef3426d35406f9c52
Jungho781 <72117787+Jungho781@users.noreply.github.com>
2020-11-12 22:39:06 -0800

    Update TextReaderController.java

--- Java3Module4/src/sample/TextReaderController.java
+++ Java3Module4/src/sample/TextReaderController.java
@@ -22,9 +22,7 @@ public class TextReaderController {
         FileChooser fileChooser=new FileChooser();
         File selectedFile=fileChooser.showOpenDialog(null);
 
-        /*I can get this do perform a word count but I suspect I need to separate the FIS
-        * into another class that will process the chosen files. How do I get the new class
-        * to remember the choices made in the first button action then process the documents?*/
+      
         if(selectedFile!=null) {
             FileInputStream fis=new FileInputStream(selectedFile);
             InputStreamReader isr=new InputStreamReader(fis);
@@ -44,33 +42,15 @@ public class TextReaderController {
             }
             textCountDisplay.getItems().add(selectedFile.getName()+": "+countWord+" words");
 
-//            textCountDisplay.getItems().add(selectedFile.getName());
         }
         else{
             System.out.println("Invalid file");
         }
     }
 
-    //need to find how to get java to count # of words in a file
-    //also need to get program to remember the files chosen
     public void Button2Action(javafx.event.ActionEvent event) throws IOException {
-//        File file=new File();
-//        FileInputStream fileInputStream=new FileInputStream(file);
-//        InputStreamReader input =new InputStreamReader(fileInputStream);
-//        BufferedReader reader=new BufferedReader(input);
-//        String line;
-//
-//        int countWord=0;
-//        int paragraphCount=1;
-//
-//        while((line=reader.readLine())!=null){
-//            if(line.equals("")){
-//                paragraphCount++;
-//            }
-//            else {
-//                String[]wordList=line.split("\\s+");
-//                countWord+=wordList.length;
-//            }
+
+
         }
 
 }
