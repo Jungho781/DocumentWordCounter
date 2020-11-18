2eb13fba75fa6343b1ee0a7a3f9171302c640e09
Jungho781 <72117787+Jungho781@users.noreply.github.com>
2020-11-17 23:50:01 -0800

    Update TextReaderController.java

--- Java3Module4/src/sample/TextReaderController.java
+++ Java3Module4/src/sample/TextReaderController.java
@@ -10,9 +10,7 @@ import javafx.stage.FileChooser;
 import java.io.*;
 
 public class TextReaderController {
-    //Probably need a shared interface object to store the documents
-    //Buttons should be called to add to or call from the object
-    //If button1 is pressed during prompt, this happens; if button2 is pressed, that happens
+ 
     @FXML
     private Button addFileBtn;
 
@@ -40,37 +38,7 @@ public class TextReaderController {
         fileLinkedList.addLast(selectedFile);
         textCountDisplay.getItems().add(selectedFile.getName());
 
-        /*I can get this do perform a word count but I suspect I need to separate the FIS
-        * into another class that will process the chosen files. How do I get the new class
-        * to remember the choices made in the first button action then process the documents?*/
-//        if(selectedFile!=null) {
-//            FileInputStream fis=new FileInputStream(selectedFile);
-//            InputStreamReader isr=new InputStreamReader(fis);
-//            BufferedReader reader=new BufferedReader(isr);
-//            String line;
-//            int countWord=0;
-//            int paragraphCount=1;
-//
-//            while ((line=reader.readLine())!=null){
-//                if(line.equals("")){
-//                    paragraphCount++;
-//                }
-//                else {
-//                    String[]wordList=line.split("\\s+");
-//                    countWord+=wordList.length;
-//                }
-//            }
-//            textCountDisplay.getItems().add(selectedFile.getName()+": "+countWord+" words");
-//
-////            textCountDisplay.getItems().add(selectedFile.getName());
-//        }
-//        else{
-//            System.out.println("Invalid file");
-//        }
-    }
-
-    //need to find how to get java to count # of words in a file
-    //also need to get program to remember the files chosen
+        
     @FXML
     public void CountWordsButtonPressed(javafx.event.ActionEvent event) throws IOException {
         countedWords.clear();
@@ -86,31 +54,11 @@ public class TextReaderController {
                 textCountDisplay.getItems().add(String.valueOf(fWCTask));
                 count++;
             }
-//            fWCTask.valueProperty().addListener((observable,oldValue,newValue)->{
-//                countedWords.add(String.valueOf(newValue));
-//                textCountDisplay.scrollTo(textCountDisplay.getItems().size());
-//            });
 
         }
         catch (Exception e){
 
         }
-//        File file=new File();
-//        FileInputStream fileInputStream=new FileInputStream(file);
-//        InputStreamReader input =new InputStreamReader(fileInputStream);
-//        BufferedReader reader=new BufferedReader(input);
-//        String line;
-//
-//        int countWord=0;
-//        int paragraphCount=1;
-//
-//        while((line=reader.readLine())!=null){
-//            if(line.equals("")){
-//                paragraphCount++;
-//            }
-//            else {
-//                String[]wordList=line.split("\\s+");
-//                countWord+=wordList.length;
-//            }
+
     }
 }
